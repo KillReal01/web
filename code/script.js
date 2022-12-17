ea550c3b3f21413c52602f29881a4200c3f5f3e6
KillReal01 <kirill.bereza.02@mail.ru>
2022-12-17 14:29:10 +0300

    update 17.12

--- code/script.js
+++ code/script.js
@@ -1,14 +1,17 @@
 var slideIndex = 0;
+var delay = 4000;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
+    //sleep(delay);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
+    //sleep(delay);
 }
 
 function showSlides() {
@@ -18,7 +21,9 @@ function showSlides() {
         slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, delay); // Change image every 4 seconds
 }
