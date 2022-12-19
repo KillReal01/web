0898b5476ee22364e7d41c2890a2a989847c097a
Alex <alexart878@gmail.com>
2022-12-19 16:38:06 +0300

    gg

--- code/script.js
+++ code/script.js
@@ -1,29 +1,37 @@
-var slideIndex = 0;
-var delay = 4000;
+/* Setting the default slide start index: */
+let slideIndex = 1;
+/* We call the function that is implemented below: */
 showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-    //sleep(delay);
+/* Increase the index by 1 - show the next slide: */
+function nextSlide() {
+    showSlides(slideIndex += 1);
 }
-
-// Thumbnail image controls
+/* Decrease the index by 1 - show the previous slide: */
+function previousSlide() {
+    showSlides(slideIndex -= 1);  
+}
+/* Set the current slide: */
 function currentSlide(n) {
     showSlides(slideIndex = n);
-    //sleep(delay);
 }
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+/* Flip function: */
+function showSlides(n) {
+    let i;
+    /* We refer to the elements with the class name "item", that is, to the pictures: */
+    let slides = document.getElementsByClassName("item");
+    
+    /* Checking the number of slides: */
+    if (n > slides.length) {
+      slideIndex = 1
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1
+    if (n < 1) {
+        slideIndex = slides.length
     }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, delay); // Change image every 4 seconds
+  
+    /* Loop through each slide in a for loop: */
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    /* Making an element block: */
+    slides[slideIndex - 1].style.display = "block";    
 }
